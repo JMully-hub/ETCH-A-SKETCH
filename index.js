1bc08454928ea5ececc8fff7911c80741cbd75f5
JMully-hub <mulligan321@hotmail.com>
2022-07-01 00:30:26 +0100

    Added some controls to the html file using drop downs, slider and check box to allow control of grid size, color and a 'water color mode'. Re organised the js file into basic functions. JS now takes the variables from the front end html controls to set variabls for grid size, color and effect. Added code to refresh to grid on each re-size. The runMain() function is called and has event listeners tied into it for each mouseover. Each 'square' div amends it's style with back ground color and opacity depending on user selection. Added rainbow color effect.

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
+function colorChange(){
+    selectedColor = document.getElementById('colorSelector').value;
+}
+
+function getGridSize(){
+    gridSize = gridSlider.value;
+    refreshGrid();
+    runMain();
+}
+
+function refreshGrid() {
+    while (sketchContainer.firstChild) {
+        sketchContainer.removeChild(sketchContainer.firstChild);
+    }
+}
+
+function runMain(){
+    for (let row = 0; row < gridSize; row++){
+        let newRow = document.createElement('div');
+        newRow.setAttribute('style', 'display: grid; column-gap: 1px;');
+        newRow.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        for (let square = 0; square < gridSize; square++){
+            let newSquare = document.createElement('div');
+            newSquare.setAttribute('style', 'background-color: white;')
+            newSquare.addEventListener('mouseover', function(){
+                
+                // colors
+                if (selectedColor === 'rainbow'){ 
+                    color = rainbowColors[Math.floor(Math.random()*rainbowColors.length)]
+                }else{
+                    color = selectedColor;
+                }
+
+                // water color effect
+                if (waterColorCB.checked){ // pencil effect (adds 10% to opacity on each pass)
+                    if (newSquare.style.opacity !== ''){
+                        newOpacity = parseFloat(newSquare.style.opacity) + 0.1;
+                    }else{
+                        newOpacity = 0.1
+                    }
+                    newSquare.setAttribute('style', `background-color: ${color}; opacity: ${newOpacity};`);
+                }else{
+                    newSquare.setAttribute('style', `background-color: ${color};`);
+                }
+            });
+            newRow.appendChild(newSquare);
+        }
+        sketchContainer.appendChild(newRow);
+    }
+
+}
+
 const sketchContainer = document.querySelector('#sketchContainer');
 
+const gridSlider = document.getElementById('gridSize')
+let output = document.getElementById('sliderOutput');
+let gridSize = gridSlider.value;
+output.innerHTML = gridSize; // default starting grid size
 
-sketchContainer.style.width = '530px'
-sketchContainer.style.height = '530px'
+let selectedColor = 'black'; // default starting color
+let color;
 
-const gridSize = 20;
+const waterColorCB = document.querySelector('#waterColor');
 
-for (let row = 0; row < gridSize; row++){
-    let newRow = document.createElement('div');
-    newRow.setAttribute('style', 'display: grid; column-gap: 1px;');
-    newRow.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-    for (let square = 0; square < gridSize; square++){
-        let newSquare = document.createElement('div');
-        newSquare.setAttribute('style', 'background-color: white;')
-        newRow.appendChild(newSquare);
+gridSlider.oninput = function() { // update the slider output value
+    output.innerHTML = this.value;
     }
-    sketchContainer.appendChild(newRow);
-}
+
+rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+document.getElementById('colorSelector').addEventListener('change', colorChange);
+document.getElementById('gridSize').addEventListener('change', getGridSize);
+runMain();
