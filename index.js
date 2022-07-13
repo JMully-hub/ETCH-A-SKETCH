6f00619b8fb3d669ae21124189b6d797a1bed92a
JMully-hub <mulligan321@hotmail.com>
2022-07-14 00:06:51 +0100

    Amended page layout to use more css grid for ease. Changed colour buttons to radio type instead of drop down for later styling ideas (push buttons). Added a "shake" function to animate the sketcher container to make it apear to shake and reset the sketcher, as in real life. Started on JS for a keyboard only mode; using the keyboard keys instead of the mouse. Added JS to disable the mouse and helpful popup hints when in keyboard mode.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function colorChange(){
-    selectedColor = document.getElementById('colorSelector').value;
+    selectedColor = document.querySelector('input[name="color"]:checked').value;
 }
 
 function getGridSize(){
@@ -24,15 +24,15 @@ function runMain(){
             newSquare.setAttribute('style', 'background-color: white;')
             
             const colorSquare = () =>{
-                    // colors
-                    if (selectedColor === 'rainbow'){ 
-                        color = rainbowColors[Math.floor(Math.random()*rainbowColors.length)]
-                    }else{
-                        color = selectedColor;
-                    }
+                // colors
+                if (selectedColor === 'rainbow'){ 
+                    color = rainbowColors[Math.floor(Math.random()*rainbowColors.length)]
+                }else{
+                    color = selectedColor;
+                }
 
-                    // water color effect (adds 10% to opacity on each pass)
-                    if (waterColorCB.checked){ 
+                // water color effect (adds 10% to opacity on each pass)
+                if (color !== 'white' && waterColorCB.checked){ // not white (eraser)
                         if (newSquare.style.opacity !== ''){
                             newOpacity = parseFloat(newSquare.style.opacity) + 0.1;
                         }else{
@@ -41,8 +41,7 @@ function runMain(){
                         newSquare.setAttribute('style', `background-color: ${color}; opacity: ${newOpacity};`);
                     }else{
                         newSquare.setAttribute('style', `background-color: ${color};`);
-                    }
-                
+                }
             };
             //colorSquare called for both mousedown and mouseover, otherwise wont color the first clicked square
             newSquare.addEventListener('mousedown', colorSquare); 
@@ -63,7 +62,7 @@ const sketchContainer = document.querySelector('#sketchContainer');
 const gridSlider = document.getElementById('gridSize')
 let output = document.getElementById('sliderOutput');
 let gridSize = gridSlider.value;
-output.innerHTML = gridSize; // display default on start
+output.innerHTML = gridSize + " x " + gridSize; // display default on start
 
 let selectedColor = 'black'; // default starting color
 let color;
@@ -71,7 +70,7 @@ let color;
 const waterColorCB = document.querySelector('#waterColor');
 
 gridSlider.oninput = function() { // update the slider output value
-    output.innerHTML = this.value;
+    output.innerHTML = this.value + " x " + this.value;
     }
 
 rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
@@ -84,4 +83,47 @@ document.addEventListener('mouseup', function(){mouseIsDown = false});
 
 document.body.setAttribute('ondragstart', "return false;"); // stop dragging behaviour
 
+// Animation  -shake the etch a sketch and clear the sketch
+document.getElementById('shake').addEventListener('click', function(){
+    document.getElementById('sketchWrapper').setAttribute('style', 'animation: shake 0.5s;')
+    refreshGrid();
+    runMain();
+});
+// once animation ended, remove the animation so it can be called again
+document.getElementById('sketchWrapper').addEventListener('animationend', function(){
+    document.getElementById('sketchWrapper').removeAttribute('style', 'animation: shake 0.5s;')
+});
+
+
 runMain();
+
+// use keyboard?
+const keyboardMode = document.querySelector('#keyboardCheck');
+keyboardMode.addEventListener('change', startKeyBoardMode);
+
+const sketchRightDiv = document.getElementById('sketchRight')
+const sketchLeftDiv = document.getElementById('sketchLeft')
+
+function startKeyBoardMode(){
+    if (this.checked){
+        refreshGrid();
+        runMain();
+        sketchRightDiv.innerText = "Click anywhere on the grid as the starting point for the keyboard";
+        sketchRightDiv.setAttribute('style', "display: block;")
+        
+        const keyBoardHelper = () =>{
+            sketchRightDiv.setAttribute('style', "display: none;")
+            sketchContainer.removeEventListener('click', keyBoardHelper);
+            sketchContainer.setAttribute('style', 'pointer-events: none'); // remove mouse input, keyboard only
+            sketchLeftDiv.innerText = "Tap the arrow keys to draw!\n\nUnselect keyboard mode to use the mouse again.";
+            sketchLeftDiv.setAttribute('style', "display: block;")
+        }
+        sketchContainer.addEventListener('click', keyBoardHelper)
+        
+    }else{
+        sketchRightDiv.setAttribute('style', "display: none;")
+        sketchLeftDiv.setAttribute('style', "display: none;")
+        sketchContainer.setAttribute('style', 'pointer-events: auto'); // reinstate mouse input
+    }
+}
+
